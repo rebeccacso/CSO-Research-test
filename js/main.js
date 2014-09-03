16d3473d28a90251dbe7c5e150e3718d7ae3e254
rebeccacso <rebecca@csoresearch.com>
2014-09-03 16:59:47 -0500

    work on fake parallax on home page

--- js/main.js
+++ js/main.js
@@ -68,3 +68,78 @@ jQuery(document).ready(function(){
 jQuery('.toggle-main-university').click(function() {
     jQuery(this).toggleClass('active');
 });
+
+
+// div height for home page - viewport
+// http://css-tricks.com/forums/topic/setting-a-div-height-to-the-window-viewport-size/
+
+
+jQuery(document).ready(function(){
+resizeDiv();
+});
+
+window.onresize = function(event) {
+resizeDiv();
+}
+
+function resizeDiv() {
+vpw = $(window).width();
+vph = $(window).height();
+jQuery('.bg-image').css({'height': vph});
+}
+
+
+	
+//home reveal text http://stackoverflow.com/questions/10555998/change-css-element-with-jquery-when-scroll-reaches-an-anchor-point
+
+function scroll_style() {
+   var window_top = $(window).scrollTop();
+   var header_top = $('#header').offset().top;
+   var system_top = $('#system').offset().top;
+   var outcomes_top = $('#outcomes').offset().top;
+   var connect_top = $('#connect').offset().top;
+   var coach_top = $('#coach').offset().top;
+
+   if (window_top > header_top){
+	   $('.bg-image.general').addClass('active');
+	   $('.bg-image.system').removeClass('active');
+	   $('.bg-image.outcomes').removeClass('active');
+	   $('.bg-image.connect').removeClass('active');
+	   $('.bg-image.coach').removeClass('active');
+   }
+
+   if (window_top > system_top){
+	   $('.bg-image.general').removeClass('active');
+	   $('.bg-image.system').addClass('active');
+	   $('.bg-image.outcomes').removeClass('active');
+	   $('.bg-image.connect').removeClass('active');
+	   $('.bg-image.coach').removeClass('active');
+   }
+   
+   if (window_top > outcomes_top){
+	   $('.bg-image.general').removeClass('active');
+	   $('.bg-image.system').removeClass('active');
+	   $('.bg-image.outcomes').addClass('active');
+	   $('.bg-image.connect').removeClass('active');	
+	   $('.bg-image.coach').removeClass('active');   
+   }
+   
+   if (window_top > connect_top){
+	   $('.bg-image.general').removeClass('active');
+	   $('.bg-image.system').removeClass('active');
+	   $('.bg-image.outcomes').removeClass('active');
+	   $('.bg-image.connect').addClass('active');	
+	   $('.bg-image.coach').removeClass('active');   
+   }
+      if (window_top > coach_top){
+	   $('.bg-image.general').removeClass('active');
+	   $('.bg-image.system').removeClass('active');
+	   $('.bg-image.outcomes').removeClass('active');
+	   $('.bg-image.connect').removeClass('active');	
+	   $('.bg-image.coach').addClass('active');   
+   }
+}
+$(function() {
+  $(window).scroll(scroll_style);
+  scroll_style();
+ });
